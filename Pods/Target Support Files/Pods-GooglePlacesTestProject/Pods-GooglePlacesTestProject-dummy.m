5d421440b76113b2c00fa40754f10e2813f62da7
Artem Chaykovsky <artem.chaykovsky@onix-systems.com>
2017-08-07 19:59:35 +0300

    Install Alamofire

---
+++ Pods/Target Support Files/Pods-GooglePlacesTestProject/Pods-GooglePlacesTestProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GooglePlacesTestProject : NSObject
+@end
+@implementation PodsDummy_Pods_GooglePlacesTestProject
+@end
---
+++ Pods/Target Support Files/Pods-GooglePlacesTestProject/Pods-GooglePlacesTestProject-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_GooglePlacesTestProjectVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GooglePlacesTestProjectVersionString[];
+
